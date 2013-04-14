b7e8723c07d7bb419b5b108602533a2b89358f42
gabrielpupr <gabriel.pupr@gmail.com>
2013-04-13 23:05:23 -0700

    Added Eda's red car package... now her update is merged with the main project. Eda make sure to use this project from now on and if you add more stuff to the Scrips/JavaScripts/move_car.js all you need to do is commit that scrip only, not the scene

--- Assets/Scrips/JavaScripts/move_car.js
+++ Assets/Scrips/JavaScripts/move_car.js
@@ -1,16 +1,42 @@
 #pragma strict
 
-var speed : float = 6.0;
-var gravity : float = 20.0;
+var speed 		: float = 6.0;	
+var gravity		: float = 20.0;
+var accelerate  : float = 0.5;
+
+var eulerAngleVelocityR : Vector3 = Vector3 (0, 100, 0);													// Holds the Euler Angle Velocity Clockwise
+var eulerAngleVelocityL : Vector3 = Vector3 (0, -100, 0);													// Holds the Euler Angle Velocity Counter-clockwise
+
 private var moveDirection : Vector3 = Vector3.zero;
-	function Update() {
-    	//var controller : CharacterController = GetComponent(CharacterController);
-        if(Input.GetAxis("Horizontal")){     
-        	transform.Translate(Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0)); 
+
+function Update() {
+																											// Quaternions are used to represent rotations
+		var deltaRotationR : Quaternion = Quaternion.Euler(eulerAngleVelocityR * Time.deltaTime);			// Right rotation
+		var deltaRotationL : Quaternion = Quaternion.Euler(eulerAngleVelocityL * Time.deltaTime);			// Left rotation
+        
+        if(Input.GetButton("RightTurn"))																	// checks if the Right Turn button is pressed
+        	rigidbody.MoveRotation(rigidbody.rotation * deltaRotationR);									// turns clockwise
+        	
+        if(Input.GetButton("LeftTurn"))																		// checks if the Left Turn button is pressed
+        	rigidbody.MoveRotation(rigidbody.rotation * deltaRotationL);									// turns counter-clockwise
+		
+        if(Input.GetAxis("Horizontal") && (!Input.GetButton("Gas")))
+        {
+        	transform.Translate(Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0));
     	}
-    	if(Input.GetAxis("Vertical")){ 
+    	if(Input.GetAxis("Vertical") && (!Input.GetButton("Gas")))
+    	{
         	transform.Translate(Vector3(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime));
     	} 
+    	
+    	if(Input.GetAxis("Vertical") && Input.GetButton("Gas") )
+    	{
+    		speed += accelerate;
+    		transform.Translate(Vector3(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime));
+    	}
+    	
+    	
+    	
 //        if (controller.isGrounded) {
 //        
 //            // We are grounded, so recalculate
