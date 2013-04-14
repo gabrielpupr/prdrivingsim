754a2b48517323ece9b921cc91b7d0edbd81ded2
Kalico <edam.cruz@gmail.com>
2013-04-13 23:42:41 -0700

    Prefab

--- Assets/Scrips/JavaScripts/move_car.js
+++ Assets/Scrips/JavaScripts/move_car.js
@@ -1,13 +1,31 @@
+/* *************** ^ *************** ^ *************** ^ ***************
+ Puerto Rico Driving Game Simulation (NAME PENDING)
+ Coding by: Edaleen Cruz, Gabriel Nieves and Francisco Ramos
+ Mentor: Arturo Geigel
+ 
+ MoveCar.js
+ Description- Coding for the character movement.  Player has the following 
+ 			  actions:
+ 				Move left or right
+ 				Move forward or backwards
+ 				Turn left or right
+ 				Accelerate
+ 				Brakes
+ * *************** ^ *************** ^ *************** ^ ***************/
+
 #pragma strict
 
-var speed 		: float = 6.0;	
-var gravity		: float = 20.0;
-var accelerate  : float = 0.5;
 
-var eulerAngleVelocityR : Vector3 = Vector3 (0, 100, 0);													// Holds the Euler Angle Velocity Clockwise
-var eulerAngleVelocityL : Vector3 = Vector3 (0, -100, 0);													// Holds the Euler Angle Velocity Counter-clockwise
+// ----- VARIABLES -----
+var speed 		: float = 6.0;			// Speed
+var gravity		: float = 20.0;			// Gravity
+var accelerate  : float = 0.5;			// Acceleration
+var decelerate	: float = 1.0;			// Deceleration
 
-private var moveDirection : Vector3 = Vector3.zero;
+var eulerAngleVelocityR : Vector3 = Vector3 (0, 100, 0);		// Holds the Euler Angle Velocity Clockwise
+var eulerAngleVelocityL : Vector3 = Vector3 (0, -100, 0);		// Holds the Euler Angle Velocity Counter-clockwise
+
+
 
 function Update() {
 																											// Quaternions are used to represent rotations
@@ -20,21 +38,30 @@ function Update() {
         if(Input.GetButton("LeftTurn"))																		// checks if the Left Turn button is pressed
         	rigidbody.MoveRotation(rigidbody.rotation * deltaRotationL);									// turns counter-clockwise
 		
-        if(Input.GetAxis("Horizontal") && (!Input.GetButton("Gas")))
+        if(Input.GetAxis("Horizontal") && (!Input.GetButton("Gas")) && (!Input.GetButton("Brake")))			// checks correct input, no break, no gas, just side
         {
-        	transform.Translate(Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0));
+        	transform.Translate(Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0));       // moves from side to side.
     	}
-    	if(Input.GetAxis("Vertical") && (!Input.GetButton("Gas")))
+    	
+    	if(Input.GetAxis("Vertical") && (!Input.GetButton("Gas")) && (!Input.GetButton("Brake")))           // checks input, no break, no gas, just front and back
     	{
-        	transform.Translate(Vector3(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime));
+        	transform.Translate(Vector3(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime));			// moves forward or backwards
     	} 
     	
-    	if(Input.GetAxis("Vertical") && Input.GetButton("Gas") )
+    	if(Input.GetAxis("Vertical") && Input.GetButton("Gas") && (!Input.GetButton("Brake")))				// checks for gas
     	{
-    		speed += accelerate;
-    		transform.Translate(Vector3(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime));
+    		speed += accelerate;																			// increases speed
+    		transform.Translate(Vector3(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime));			// performs the transformation
     	}
     	
+    	if(Input.GetAxis("Vertical") && (!Input.GetButton("Gas")) && Input.GetButton("Brake"))				// checks if the brake is pressed
+    	{
+    		speed -= decelerate;																			// decreases speed
+    		if(speed < 0)																					// checks if the speed is at negative
+    			speed = 0;																					// if speed is lower than 0, set speed to 0
+    		transform.Translate(Vector3(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime));			// TRANSFORMATION... POW.
+    	}
+}    	
     	
     	
 //        if (controller.isGrounded) {
@@ -56,6 +83,3 @@ function Update() {
         
         // Move the controller
         //controller.Move(moveDirection * Time.deltaTime);
-        
-         
-    }
